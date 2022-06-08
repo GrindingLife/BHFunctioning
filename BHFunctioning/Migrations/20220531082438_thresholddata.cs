01568ee6027331ef24877cab795a1f4e39383b92
Bill Tran <bill.tran54499@gmail.com>
2022-06-08 20:38:34 +1000

    fixed alert message displaying in second form

---
+++ BHFunctioning/Migrations/20220531082438_thresholddata.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BHFunctioning.Migrations
+{
+    public partial class thresholddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Threshold_50",
+                table: "HealthData",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Threshold_60",
+                table: "HealthData",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Threshold_70",
+                table: "HealthData",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Threshold_50",
+                table: "HealthData");
+
+            migrationBuilder.DropColumn(
+                name: "Threshold_60",
+                table: "HealthData");
+
+            migrationBuilder.DropColumn(
+                name: "Threshold_70",
+                table: "HealthData");
+        }
+    }
+}
